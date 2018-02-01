9d30d8848a48c793900953180ea211d276c2172c
Nikhil Patnaik <nikhilpatnaik@medwlanB58A.kent.ac.uk>
2018-02-01 13:14:40 +0000

    refactored version with sumtotal and meantotal functions created and being called in correlation function, replicated code blocks removed

--- plumber.r
+++ plumber.r
@@ -26,114 +26,86 @@ function(dataset1,
   options(scipen=999)
   
   conv <- as.data.frame(dataset1)
-  conv3 <- as.data.frame(dataset1)
-  conv5 <- as.data.frame(dataset1)
-  conv7 <- as.data.frame(dataset1)
-  
-  #1 Hour
-  df1 <- conv %>%
-    mutate(startdate = ymd_hms(startdate),
-           enddate = ymd_hms(enddate)) %>%
-    mutate(total = as.numeric(total)) %>%
-    mutate(month_name = month(startdate, label = TRUE)) %>%
-    group_by(hour = floor_date(startdate, "hour")) %>%
-    summarize(total = sum(total))
-
   conv2 <- as.data.frame(dataset2)
-
-  df2 <- conv2 %>%
-    mutate(startdate = ymd_hms(startdate),
-           enddate = ymd_hms(enddate)) %>%
-    mutate(total = as.numeric(total)) %>%
-    mutate(month_name = month(startdate, label = TRUE)) %>%
-    group_by(hour = floor_date(startdate, "hour")) %>%
-    summarize(total = sum(total))
-
-  df3 <- df2[(df2$hour %in% df1$hour), ]
-  df4 <- df1[(df1$hour %in% df2$hour), ]
   
-  #1 Day
-  df5 <- conv3 %>%
-    mutate(startdate = ymd_hms(startdate),
-           enddate = ymd_hms(enddate)) %>%
-    mutate(total = as.numeric(total)) %>%
-    mutate(month_name = month(startdate, label = TRUE)) %>%
-    group_by(hour = floor_date(startdate, "1 day")) %>%
-    summarize(total = sum(total))
-
-
-  conv4 <- as.data.frame(dataset2)
-
-  df6 <- conv4 %>%
-    mutate(startdate = ymd_hms(startdate),
-           enddate = ymd_hms(enddate)) %>%
-    mutate(total = as.numeric(total)) %>%
-    mutate(month_name = month(startdate, label = TRUE)) %>%
-    group_by(hour = floor_date(startdate, "1 day")) %>%
-    summarize(total = sum(total))
-
-  df7 <- df6[(df6$hour %in% df5$hour), ]
-  df8 <- df5[(df5$hour %in% df6$hour), ]
-
-  # #7 Days
-  df9 <- conv5 %>%
-    mutate(startdate = ymd_hms(startdate),
-           enddate = ymd_hms(enddate)) %>%
-    mutate(total = as.numeric(total)) %>%
-    mutate(month_name = month(startdate, label = TRUE)) %>%
-    group_by(hour = floor_date(startdate, "7 days")) %>%
-    summarize(total = sum(total))
-
-  conv6 <- as.data.frame(dataset2)
-
-  df10 <- conv6 %>%
-    mutate(startdate = ymd_hms(startdate),
-           enddate = ymd_hms(enddate)) %>%
-    mutate(total = as.numeric(total)) %>%
-    mutate(month_name = month(startdate, label = TRUE)) %>%
-    group_by(hour = floor_date(startdate, "7 days")) %>%
-    summarize(total = sum(total))
-
-  df11 <- df10[(df10$hour %in% df9$hour), ]
-  df12 <- df9[(df9$hour %in% df10$hour), ]
-
-
-  # #30 Days
-  df13 <- conv7 %>%
-    mutate(startdate = ymd_hms(startdate),
-           enddate = ymd_hms(enddate)) %>%
-    mutate(total = as.numeric(total)) %>%
-    mutate(month_name = month(startdate, label = TRUE)) %>%
-    group_by(hour = floor_date(startdate, "30 days")) %>%
-    summarize(total = sum(total))
-
-  conv8 <- as.data.frame(dataset2)
-
-  df14 <- conv8 %>%
-    mutate(startdate = ymd_hms(startdate),
-           enddate = ymd_hms(enddate)) %>%
-    mutate(total = as.numeric(total)) %>%
-    mutate(month_name = month(startdate, label = TRUE)) %>%
-    group_by(hour = floor_date(startdate, "30 days")) %>%
-    summarize(total = sum(total))
-
-  df15 <- df14[(df14$hour %in% df13$hour), ]
-  df16 <- df13[(df13$hour %in% df14$hour), ]
+  if(parameter1 == 'heartrate' || parameter1 == 'sleepheartrate'){
+    df1 <- meantotal(conv, 'hour')
+    df2 <- meantotal(conv, '1 day')
+    df3 <- meantotal(conv, '7 days')
+    df4 <- meantotal(conv, '30 days')
+  }else{
+    df1 <- sumtotal(conv, 'hour')
+    df2 <- sumtotal(conv, '1 day')
+    df3 <- sumtotal(conv, '7 days')
+    df4 <- sumtotal(conv, '30 days')
+  }
+  
+  if(parameter2 == 'heartrate' || parameter2 == 'sleepheartrate'){
+    df10 <- meantotal(conv2,'hour')
+    df20 <- meantotal(conv2, '1 day')
+    df30 <- meantotal(conv2, '7 days')
+    df40 <- meantotal(conv2, '30 days')
+    
+  }else{
+    df10 <- sumtotal(conv2, 'hour')
+    df20 <- sumtotal(conv2, '1 day')
+    df30 <- sumtotal(conv2, '7 days')
+    df40 <- sumtotal(conv2, '30 days')
+  }
+  
+  
+  df100 <- df10[(df10$hour %in% df1$hour), ]
+  df200 <- df1[(df1$hour %in% df10$hour), ]
+  View(df100)
+  View(df200)
+  
+  df300 <- df20[(df20$hour %in% df2$hour), ]
+  df400 <- df2[(df2$hour %in% df20$hour), ] 
+  View(df300)
+  View(df400)
+  
+  df500 <- df30[(df30$hour %in% df3$hour), ]
+  df600 <- df3[(df3$hour %in% df30$hour), ] 
+  View(df500)
+  View(df600)
+  
+  df700 <- df40[(df40$hour %in% df4$hour), ]
+  df800 <- df4[(df4$hour %in% df40$hour), ] 
+  View(df700)
+  View(df800)
+  
   
   title <-
     paste("Correlation between", parameter1, "and", parameter2, "over ...", sep = " ")
   
   attach(mtcars)
   par(mfrow=c(2,2), mai=c(0.8,1,2,1), cex=1.5)
-  plot(df3$total, df4$total, type = "p", ann = FALSE)
+  plot(df100$total, df200$total, type = "p", ann = FALSE)
   title("1 Hour")
-  plot(df7$total, df8$total, type = "p", ann = FALSE)
+  plot(df300$total, df400$total, type = "p", ann = FALSE)
   title("1 Day")
-  plot(df11$total, df12$total, type = "p", ann = FALSE)
+  plot(df500$total, df600$total, type = "p", ann = FALSE)
   title("1 Week")
-  plot(df10$total, df12$total, type = "p", ann = FALSE)
+  plot(df700$total, df800$total, type = "p", ann = FALSE)
   title("1 Month")
   mtext(title, side = 3, line = -2, outer=TRUE, cex = 2.5)
+  
 }
 
+sumtotal <- function(conv, duration){
+  conv %>%
+    mutate(startdate = ymd_hms(startdate)) %>%
+    mutate(total = as.numeric(total)) %>%
+    mutate(month_name = month(startdate, label = TRUE)) %>%
+    group_by(hour = floor_date(startdate, duration)) %>%
+    summarize(total = sum(total))
+}
 
+meantotal <- function(conv, duration){
+  conv %>%
+    mutate(startdate = ymd_hms(startdate)) %>%
+    mutate(total = as.numeric(total)) %>%
+    mutate(month_name = month(startdate, label = TRUE)) %>%
+    group_by(hour = floor_date(startdate, duration)) %>%
+    summarize(total = mean(total))
+}
